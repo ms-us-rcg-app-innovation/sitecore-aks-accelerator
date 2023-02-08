bc81c94266b1f83f9d3a13a6075c824484a02e71
msftph <65667413+msftph@users.noreply.github.com>
2023-02-08 15:05:18 -0500

    moves secrets to yml

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,231 +1,56 @@
-locals {
-  // name: the name of the secret
-  // type: one of ("value", "password", "certificate")
-  secrets = [
-    {
-      name     = "sitecore-license",
-      type     = "license", // changed from "value" since license needs to be a specific/static value
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-admin-password",
-      type     = "password",
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-telerik-encryption-key",
-      type     = "value",
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-identity-certificate",
-      type     = "certificate",
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-identity-certificate-password",
-      type     = "password",
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-identity-secret",
-      type     = "password",
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-reporting-api-key",
-      type     = "password",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-solr-connection-string",
-      type     = "value",
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-solr-connection-string-xdb",
-      type     = "value",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-database-username",
-      type     = "value",
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-database-server-name",
-      type     = "value",
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-database-password",
-      type     = "password",
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-database-elastic-pool-name",
-      type     = "value",
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-core-database-username",
-      type     = "value",
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-core-database-password",
-      type     = "password",
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-master-database-username",
-      type     = "value",
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-master-database-password",
-      type     = "password",
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-web-database-username",
-      type     = "value",
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-web-database-password",
-      type     = "password",
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-forms-database-username",
-      type     = "value",
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-forms-database-password",
-      type     = "password",
-      topology = ["XM1", "XP1"]
-    },
-    {
-      name     = "sitecore-exm-master-database-username",
-      type     = "value",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-exm-master-database-password",
-      type     = "password",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-messaging-database-username",
-      type     = "value",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-messaging-database-password",
-      type     = "password",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-marketing-automation-username",
-      type     = "value",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-marketing-automation-password",
-      type     = "password",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-processing-engine-storage-database-username",
-      type     = "value",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-processing-engine-storage-database-password",
-      type     = "password",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-processing-engine-tasks-database-password",
-      type     = "password",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-processing-engine-tasks-database-username",
-      type     = "value",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-processing-pools-database-password",
-      type     = "password",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-processing-pools-database-username",
-      type     = "value",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-processing-tasks-database-password",
-      type     = "password",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-processing-tasks-database-username",
-      type     = "value",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-reference-data-database-password",
-      type     = "password",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-reference-data-database-username",
-      type     = "value",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-reporting-database-password",
-      type     = "password",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-reporting-database-username",
-      type     = "value",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-collection-shardmapmanager-database-username",
-      type     = "value",
-      topology = ["XP1"]
-    },
-    {
-      name     = "sitecore-collection-shardmapmanager-database-password",
-      type     = "password",
-      topology = ["XP1"]
-    }
-  ]
+locals{
+  secrets_yaml = file("${path.module}/secrets.yml")
+  secrets = yamldecode(secrets_yaml)
 }
 
-resource "random_password" "secrets" {
+module "password" {
+  source = "./modules/key-vault-password"
   for_each = {
     for secret in local.secrets :
     secret.name => secret
     if secret.type == "password"
   }
-  length = 16
 }
 
-resource "azurerm_key_vault_secret" "secrets_passwords" {
-  for_each = {
-    for secret in local.secrets :
-    secret.name => secret
-    if secret.type == "password"
-  }
-  name         = each.value.name
-  value        = random_password.secrets[each.value.name].result
+module "certificate" {  
+  source = "./modules/key-vault-certificate"
+
+  name = each.key
+  common_name = each.value.options.common_name
+  organization = each.value.options.organization
   key_vault_id = azurerm_key_vault.default.id
+
+  for_each = {
+    for secret in local.secrets :
+    secret.name => secret
+    if secret.type == "certificate"
+  }
+}
+
+module "value" {
+  source = "./modules/key-vault-value"
+
+  name = each.key
+  common_name = each.value.options.common_name
+  organization = each.value.options.organization
+  key_vault_id = azurerm_key_vault.default.id
+
+
+  for_each = {
+    for secret in local.secrets :
+    secret.name => secret
+    if secret.type == "value"
+  }
+}
+
+module "certificate_authority"{
+  source = "./modules/key-vault-certificate-authority"
+
+
+
+  for_each = {
+    for secret in local.secrets :
+    secret.name => secret
+    if secret.type == "certificate-authority"
+  }
 }
